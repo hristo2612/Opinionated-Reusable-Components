dffc81b53d13ffecf65d3effa36d780548eba8ac
hristo2612 <hristoapps@gmail.com>
2018-10-06 20:18:10 +0300

    Add DiffPreview Code Block Component
    - empty for now
    
    will be complete in a jiffy

---
+++ src/components/DiffPreview/DiffPreview.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class DiffPreview extends Component {
+  static propTypes = {
+    code: PropTypes.string.isRequired,
+    theme: PropTypes.string
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
